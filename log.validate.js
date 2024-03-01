8a4023e2d707206447aeb6047cd5dff058a45146
Sumit Suthar <ssuthar@newrelic.com>
2024-03-01 16:03:35 +0530

    minor change in log

--- log.validate.js
+++ log.validate.js
@@ -1,7 +1,7 @@
 async function validate(req, res, next) {
   for (let i = 0; i < 1000; i++) {
     setTimeout(() => {
-      console.log("Run after " + (i + 1) + " seconds");
+      console.log("Run after " + (i + 1) );
     }, 1000);
   }
   next();
